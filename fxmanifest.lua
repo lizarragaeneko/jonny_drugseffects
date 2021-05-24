9fce735282a2621f8ee124b25bc7e5ff9767a5d4
lizarragaeneko <67381650+lizarragaeneko@users.noreply.github.com>
2021-05-24 16:51:10 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+games{ 'gta5' }
+server_scripts{
+'config.lua',
+'server.lua'
+}
+client_scripts{
+'config.lua',
+'client.lua'
+}
+fx_version 'adamant'
+author 'ElNekoDev :)'
+files{
+   'ui/ui.html',
+   'ui/main.css',
+   'ui/main.js',
+   'ui/img/mario.png',
+   'ui/img/yoshi.png'
+   }
+ui_page 'ui/ui.html'
